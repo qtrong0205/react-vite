8e0a6d7a3f56e443352e0b9f192854c42061a146
qtrong0205 <quoctrong02052006@gmail.com>
2025-08-28 15:29:58 +0700

    video 30 fragment, css

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,17 @@
 // () => {}
 // tên của component phải viết hoa chữ cái đầu
 // component = HTML + CSS + JS
+// class trong react sẽ được khai báo là className
+import './style.css'
+
 const MyComponent = () => {
   return (
-    <div>Học component cùng tôi update</div>
+    // jsx chỉ có một phần tử cha duy nhất
+    // đây là cú pháp khai báo fragment, khi lên browser sẽ không có thẻ nào bọc ngoài 2 thẻ div
+    <> 
+      <div>Học component cùng tôi update</div>
+      <div className="child" style={{borderRadius : "10px"}}>child</div>
+    </>
   );
 }
 
