88c565437b976ea3165e6a5fe7c1e7ffa0b27b39
qtrong0205 <quoctrong02052006@gmail.com>
2025-08-28 16:57:55 +0700

    video 32 tạo giao diện todo

---
+++ src/components/todo/todoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+      <div className='todo-data'> 
+        <div>Learning React</div>
+        <div>Watch youtube</div>
+      </div>
+    )
+}
+
+export default TodoData
