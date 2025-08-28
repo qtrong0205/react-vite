eff0b2be4d04365b437391f3fa7461db55ae6dc7
qtrong0205 <quoctrong02052006@gmail.com>
2025-08-28 15:43:36 +0700

    video 31 variable

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -5,11 +5,19 @@
 import './style.css'
 
 const MyComponent = () => {
+  const myName = "Trọng" //string
+  // const myName = 25 //number
+  const obj = {
+    name: "Trọng",
+    age: 18
+  }
+  const arr = [1, 2, 3]
   return (
     // jsx chỉ có một phần tử cha duy nhất
     // đây là cú pháp khai báo fragment, khi lên browser sẽ không có thẻ nào bọc ngoài 2 thẻ div
     <> 
-      <div>Học component cùng tôi update</div>
+      <div>Học component cùng {JSON.stringify(arr)} update</div>
+      <div>{console.log("Trọng")}</div>
       <div className="child" style={{borderRadius : "10px"}}>child</div>
     </>
   );
