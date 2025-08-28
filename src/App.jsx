63cb90cb077870301e85e57cc67e316872fe716d
qtrong0205 <quoctrong02052006@gmail.com>
2025-08-28 18:24:21 +0700

    video 34 props

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import TodoNew from './components/todo/todoNew'
 import reactLogo from './assets/react.svg'
 
 const App = () => {
- 
-
+  const trong = "Trọng"
+  const age = 18
+  const data = {
+    country: 'Việt Nam',
+    city: 'Cần Thơ'
+  }
+  // {key: value}
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew/>
-      <TodoData/>
+      <TodoData 
+      name = {trong}
+      age = {age}
+      data = {data}
+      />
       <div className='todo-img'>
         <img src={reactLogo} alt="" className='logo'/>
       </div>
--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,8 +1,16 @@
-const TodoData = () => {
+// cách code khác: const TodoData = ({name}) => {
+// lấy ra biến name từ props luôn 
+
+const TodoData = (props) => {
+    // props là một biến object
+    // destructuring object
+    const {name, age, data} = props
     return (
       <div className='todo-data'> 
         <div>Learning React</div>
         <div>Watch youtube</div>
+        <div>My name is {name}</div>
+        <div>My hometown is {data.city}</div>
       </div>
     )
 }
