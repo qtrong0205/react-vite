a1f72066d521511e995615190f3889ce89a28965
qtrong0205 <quoctrong02052006@gmail.com>
2025-08-29 12:57:25 +0700

    video 36

--- src/components/todo/todoNew.jsx
+++ src/components/todo/todoNew.jsx
@@ -1,12 +1,21 @@
 const TodoNew = (props) => {
   const {addNewToDo} = props
   // addNewToDo("Trọng")
-    return (
-      <div className='todo-new'>
-        <input type="text" placeholder='Enter your task' />
-        <button>Add</button>
-      </div>
-    )
+  const handleClick = () =>{
+    alert("Click me")
+  }
+
+  const handleOnChange = (name) =>{
+    console.log(name)
+  }
+  return (
+    <div className='todo-new'>
+      <input type="text" placeholder='Enter your task' 
+      onChange={(event) => handleOnChange(event.target.value)}/>
+      <button style={{cursor:"pointer"}}
+      onClick={handleClick}>Add</button>
+    </div>
+  )
 }
 
 export default TodoNew
