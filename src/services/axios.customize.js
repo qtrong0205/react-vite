a8954929f6eeb90995cf16a78bfd34ba426f6d60
qtrong0205 <quoctrong02052006@gmail.com>
2025-09-14 10:08:31 +0700

    thêm progress bar

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,4 +1,11 @@
 import axios from "axios";
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+});
+
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -10,6 +17,7 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
+    NProgress.start()
     if (typeof window !== "undefined" && window
         && window.localStorage
         && window.localStorage.getItem('access_token')) {
@@ -18,6 +26,7 @@ instance.interceptors.request.use(function (config) {
     // Do something before request is sent
     return config;
 }, function (error) {
+    NProgress.done()
     // Do something with request error
     return Promise.reject(error);
 });
@@ -25,11 +34,13 @@ instance.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+    NProgress.done()
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     if (response.data && response.data.data) return response.data
     return response;
 }, function (error) {
+    NProgress.done()
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     if (error.response && error.response.data) return error.response.data
